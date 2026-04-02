660c14ab9fde2495fada35d8d148561e9b933c42
ailshi03ne-boop <ailshi03ne@gmail.com>
2026-04-02 14:53:51 +0900

    Q４修正

--- php-practice_1.php
+++ php-practice_1.php
@@ -22,14 +22,10 @@ echo "現在時刻は".date("Y年m月d日 h時i分s秒");
 
 $device = "mac";
 
-if ($device == "windows") {
-  echo "使用OSは、windowsです。";
+if ($device == "windows" || $device == "mac") {
+    echo "使用OSは、".$device."です。";
 } else {
-  if ($device == "mac") {
-    echo "使用OSは、macです。";
-  } else {
     echo "どちらでもありません。";
-  }
 }
 
 
